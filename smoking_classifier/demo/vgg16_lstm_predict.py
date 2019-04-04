4bcb3e0023949c7f8ff580094ea39d45d105595e
sharmaritvik60 <sharmaritvik60@gmail.com>
2019-04-04 13:25:40 +0530

    Added vgg16 LSTM model

---
+++ smoking_classifier/demo/vgg16_lstm_predict.py
@@ -0,0 +1,48 @@
+import numpy as np
+from keras import backend as K
+import sys
+import os
+
+
+def main():
+    K.set_image_dim_ordering('tf')
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+    from keras_video_classifier.library.recurrent_networks import VGG16LSTMVideoClassifier
+    from keras_video_classifier.library.utility.ucf.UCF101_loader import load_ucf, scan_ucf_with_labels
+
+    vgg16_include_top = True
+    data_dir_path = os.path.join(os.path.dirname(__file__), 'very_large_data')
+    model_dir_path = os.path.join(os.path.dirname(__file__), 'models', 'UCF-101')
+    config_file_path = VGG16LSTMVideoClassifier.get_config_file_path(model_dir_path,
+                                                                     vgg16_include_top=vgg16_include_top)
+    weight_file_path = VGG16LSTMVideoClassifier.get_weight_file_path(model_dir_path,
+                                                                     vgg16_include_top=vgg16_include_top)
+
+    np.random.seed(42)
+
+    load_ucf(data_dir_path)
+
+    predictor = VGG16LSTMVideoClassifier()
+    predictor.load_model(config_file_path, weight_file_path)
+
+    videos = scan_ucf_with_labels(data_dir_path, [label for (label, label_index) in predictor.labels.items()])
+
+    video_file_path_list = np.array([file_path for file_path in videos.keys()])
+    np.random.shuffle(video_file_path_list)
+
+    correct_count = 0
+    count = 0
+
+    for video_file_path in video_file_path_list:
+        label = videos[video_file_path]
+        predicted_label = predictor.predict(video_file_path)
+        print('predicted: ' + predicted_label + ' actual: ' + label)
+        correct_count = correct_count + 1 if label == predicted_label else correct_count
+        count += 1
+        accuracy = correct_count / count
+        print('accuracy: ', accuracy)
+
+
+if __name__ == '__main__':
+    main()
